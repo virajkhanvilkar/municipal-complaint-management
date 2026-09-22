cf913e0f02f3eeb0c8f24d8e2eebf92357e11e1a
Viraj Khanvilkar <khanvilkarviraj@gmail.com>
2026-09-22 16:05:47 +0530

    Add Spring Boot project setup

---
+++ src/main/java/com/municipal/complaint_management/ComplaintManagementApplication.java
@@ -0,0 +1,13 @@
+package com.municipal.complaint_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintManagementApplication.class, args);
+	}
+
+}
