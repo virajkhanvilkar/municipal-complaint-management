b37c9fdd273fd7a29c0d7a172ec1eef95f27c414
ramaikulkarni <ramaikulkarni3@gmail.com>
2026-09-23 12:54:20 +0530

    Fix test package structure

--- src/test/java/com/municipal/complaint_management/ComplaintManagementApplicationTests.java
+++ src/test/java/com/municipal/complaint_management/ComplaintManagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.municipal.complaintmanagement;
+package com.municipal.complaint_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ComplaintManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
